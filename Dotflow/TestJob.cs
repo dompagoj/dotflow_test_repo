0e01aedd521c7dad4366c00507289284a4a7af1a
Domagoj Zivanovic <domagoj.zivanovic@gmail.com>
2024-10-18 21:25:32 +0200

    Adds dotflow test job

---
+++ Dotflow/TestJob.cs
@@ -0,0 +1,17 @@
+﻿using DotFlowCI;
+
+namespace Dotflow;
+
+[DotFlowJob]
+public class TestJob : Job
+{
+    public TestJob()
+    {
+        Name = "Test job";
+
+        StartOn(opts =>
+        {
+            opts.GitPush(pushOpts => ValueTask.FromResult(pushOpts.Branch == "main"));
+        });
+    }
+}
